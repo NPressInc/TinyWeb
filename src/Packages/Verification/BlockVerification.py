769daee6d73cc45d841d940b5cd6c24953c892c2
floydw1234 <floydw1234@gmail.com>
2021-11-06 23:38:39 -0600

    added dummy functions to block verifyer based on writings

--- src/Packages/Verification/BlockVerification.py
+++ src/Packages/Verification/BlockVerification.py
@@ -13,4 +13,37 @@ class BlockVerification:
     def VerifyTransaction(transaction):
         print("TBI")
 
+    def addressHasPermissionToContact(pubKey, Action, RecievingParty):
+        print("TBI")
+
+    def addressHasPermissionToSeeFile(pubKey, Action, FileHash):
+        print("TBI")
+
+    def addressHasPermissionToVote(pubKey, Action, electionId):
+        print("TBI")
+
+    def addressHasPermissionToProposeVote(pubKey, Action):
+        print("TBI")
+
+    def addressHasPermissionToCreateGroup(pubKey, Action):
+        print("TBI")
+
+    def addressHasPermissionToModifyGroup(pubKey, Action):
+        print("TBI")
+
+    def addressHasPermissionToModifyRole(pubKey, Action):
+        print("TBI")
+
+    def addressHasPermissionToCreateRole(pubKey, Action):
+        print("TBI")
+
+    def addressHasPermissionToCreateRole(pubKey, Action):
+        print("TBI")
+        
+    def getPermissionsFromRole(roleName):
+        print("TBI")
+
+    
+
+
     
